4bddb377bc9ef34560ec078882f12352b4d275f5
Tamirat Dereje <91497780+tamiratdereje@users.noreply.github.com>
2023-02-02 09:59:59 +0300

    Update number-of-ways-to-select-buildings.py

--- number-of-ways-to-select-buildings.py
+++ number-of-ways-to-select-buildings.py
@@ -1,17 +1,37 @@
 class Solution:
-    def minimumOperations(self, nums: List[int]) -> int:
+    def numberOfWays(self, s: str) -> int:
+        dp = {'0' : 0, '1' : 0, '01': 0, '10':0, '010':0, '101':0}
         
+        for i in range(len(s)):
+            if s[i] == '0':
+                dp['10'] += dp['1']
+                dp['0'] += 1
+                dp['010'] += dp['01'] 
+                
+            else:
+                dp['01'] += dp['0']
+                dp['1'] += 1
+                dp['101'] += dp['10']
         
-        heapq.heapify(nums)
+        return dp['101'] + dp['010']
+    
+            
         
-        ans = 0
-        subt = 0
+        @lru_cache(None)
+        def dfs(prev, ind, count_):
+            
+            if count_ == 3:
+                return 1
+            
+            if ind >= len(s):
+                return 0
+            
+            res = 0
+            
+            res += dfs(prev, ind + 1, count_)
+            if prev != s[ind]:
+                res += dfs(s[ind], ind + 1, count_ + 1)
+                        
+            return res
         
-        while nums:
-            # print(nums)
-            cur = heapq.heappop(nums)
-            if (cur - subt) > 0:
-                ans +=  1
-            subt += cur - subt
-        
-        return ans
+        # return dfs(-1, 0, 0)
